df38312ff3287d6c04d73fc59a379ed5cf53372a
chris-marie <cmhamilton2.718@gmail.com>
2015-01-08 14:04:33 -0800

    added basic event schema and created sample instance of model

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
-var express = require('express');
+	var express = require('express');
 var router = express.Router();
 
 // mongoose handling
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
-var Event = mongoose.model('Event', {
+///// WHY IS THERE CURRENTLY ONLY A MODEL, AND NOT ALSO A SCHEMA?
+
+// SCHEMA USED TO STORE STRUCTURE OF DOCUMENTS WITHIN A COLLECTION
+var eventSchema = mongoose.schema ({ 
     title: {
 	type: String,
 	unique: true
@@ -14,16 +17,41 @@ var Event = mongoose.model('Event', {
     location: String,
     description: String,
     categories: String,  // change categories to checkboxes and add and other
-    companies: String,
     sponsor: String,
+    companies: String,
     maxCapacity: String,
-    // when Register is working, take number of registered attendants and get remainning spaces
+    /// when Register is working, take number of registered attendants and get remainning spaces
     // currentAttendants: number from Register
     // remainingSpaces: maxCapacity - currentAttendants
 });
 
 
-/* GET home page. */
+// MODELS ARE USED TO CREATE INSTANCES OF DATA THAT WILL BE STORED IN DOCUMENTS
+var Events = mongoose.model('Events', eventSchema});
+
+// SAMPLE MOVIE DATA BY INATIATING AN EVENTS MODEL
+
+var eventSample = new Events({ 
+    title: 'SJU Job Fair',
+    timestamp: new Date();   ///// IS THIS the time the event is created or the data of event?
+    location: 'San Jose University, 1 Washington Sq, San Jose, CA 95192',
+    description: 'Job fair for students looking for CS and design work',
+    categories: 'UI, UX, backend, end to end, big data',
+    sponsor: 'Amazon',
+    commpanies: 'Amazon, Google, Walmart, Sony, Hitachi, Square', 
+    maxCapacity: '300'
+});
+
+eventSample.save(function( err, eventSample) {
+    if (err) return console.log('ERROR on eventSample SAVE: ' + err);
+    else {
+	console.log('eventSample: ' + eventSample);
+	// Create a div and display on the current events sidebar
+    }
+});
+
+/* GET home page.   */
+///// DOES THIS COME BEFORE OR AFTER all the main page work, search result display, and form?
 router.get('/', function(req, res) {
   res.render('index', { title: 'Events by Hiring Boost' });
 });
